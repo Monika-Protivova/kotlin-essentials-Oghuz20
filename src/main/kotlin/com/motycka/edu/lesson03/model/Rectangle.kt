f5828b90c0f2797d25ddfff4b75c76935ba4a08d
Oghuz Hasanli <oguzhsnli2@gmail.com>
2025-06-30 21:15:37 +0400

    Rectangle.kt

--- src/main/kotlin/com/motycka/edu/lesson03/model/Rectangle.kt
+++ src/main/kotlin/com/motycka/edu/lesson03/model/Rectangle.kt
@@ -1,7 +1,20 @@
 package com.motycka.edu.lesson03.model
 
-/*
- Implement Rectangle class that implements Shape2D and Quadrilateral interfaces.
-    - It should have a constructor that takes two parameters for width and length.
-    - It should override all methods from Shape2D and Quadrilateral interfaces
- */
+import com.motycka.edu.lesson03.Quadrilateral
+import com.motycka.edu.lesson03.Shape2D
+import com.motycka.edu.lesson03.Shape3D
+
+open class Rectangle(
+    private val _width: Double,
+    private val _length: Double
+) : Shape2D, Quadrilateral {
+
+    // Public overrides for interface properties
+    override val width: Double get() = _width
+    override val length: Double get() = _length
+
+    override fun area(): Double = _width * _length
+    override fun perimeter(): Double = 2 * (_width + _length)
+
+    override fun to3D(depth: Double): Shape3D = Cuboid(_width, _length, depth)
+}
