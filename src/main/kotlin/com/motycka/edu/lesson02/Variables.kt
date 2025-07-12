ecba81882ce783b4f56cc9b7e6abc4ef11d18f0f
Oghuz Hasanli <oguzhsnli2@gmail.com>
2025-07-12 22:03:53 +0400

    Variables.kt

--- src/main/kotlin/com/motycka/edu/lesson02/Variables.kt
+++ src/main/kotlin/com/motycka/edu/lesson02/Variables.kt
@@ -1,20 +1,20 @@
 package com.motycka.edu.lesson02
 
-// Constants
 const val espresso: String = "Espresso"
 const val cappuccino: String = "Cappuccino"
 const val espressoPrice: Double = 2.5
 const val cappuccinoPrice: Double = 3.0
 
-// ✅ NEW FUNCTION for testing
-fun calculateTotalPrice(orders: List<String>): Double {
-    val espressoQuantity = orders.count { it == espresso }
-    val cappuccinoQuantity = orders.count { it == cappuccino }
-
-    return (espressoQuantity * espressoPrice) + (cappuccinoQuantity * cappuccinoPrice)
-}
-
 fun main(args: Array<String>) {
-    val totalPrice = calculateTotalPrice(args.toList())
+    val espressoQuantity = args.count { it == espresso }
+    val cappuccinoQuantity = args.count { it == cappuccino }
+
+    val totalPrice = (espressoQuantity * espressoPrice) + (cappuccinoQuantity * cappuccinoPrice)
     println(totalPrice)
 }
+
+fun variables(espressoQuantity: Int, cappuccinoQuantity: Int) {
+    val totalPrice = (espressoQuantity * espressoPrice) + (cappuccinoQuantity * cappuccinoPrice)
+    println(totalPrice)
+}
+
