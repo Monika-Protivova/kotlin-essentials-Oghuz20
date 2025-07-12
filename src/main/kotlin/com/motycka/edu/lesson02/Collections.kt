2d31eeea4813fc7e4660ea0676ed0e725e6c6f63
Oghuz Hasanli <oguzhsnli2@gmail.com>
2025-07-12 22:19:17 +0400

    Collections.kt

--- src/main/kotlin/com/motycka/edu/lesson02/Collections.kt
+++ src/main/kotlin/com/motycka/edu/lesson02/Collections.kt
@@ -1,6 +1,6 @@
 package com.motycka.edu.lesson02
 
-// Define shared coffee constants
+// Coffee names
 const val ESPRESSO = "Espresso"
 const val DOUBLE_ESPRESSO = "Double Espresso"
 const val CAPPUCCINO = "Cappuccino"
@@ -9,6 +9,14 @@ const val MACCHIATO = "Macchiato"
 const val MOCHA = "Mocha"
 const val FLAT_WHITE = "Flat White"
 const val AMERICANO = "Americano"
+
+// Coffee prices 
+const val ESPRESSO_PRICE = 2.5
+const val DOUBLE_ESPRESSO_PRICE = 3.0
+const val CAPPUCCINO_PRICE = 3.0
+const val LATTE_PRICE = 3.5
+const val AMERICANO_PRICE = 2.0
+const val FLAT_WHITE_PRICE = 3.2
 
 // Define menu
 val coffeeMenu = listOf(
