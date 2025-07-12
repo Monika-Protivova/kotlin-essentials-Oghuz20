d375c7bdcfe59233c7c67d5b1c3e0f67ac3ac3c4
Oghuz Hasanli <oguzhsnli2@gmail.com>
2025-07-12 22:34:42 +0400

    Constants.kt

--- src/main/kotlin/com/motycka/edu/lesson02/Constants.kt
+++ src/main/kotlin/com/motycka/edu/lesson02/Constants.kt
@@ -1,6 +1,6 @@
 package com.motycka.edu.lesson02
-import com.motycka.edu.lesson02.Constants.*
 
+// Coffee names
 const val ESPRESSO = "Espresso"
 const val DOUBLE_ESPRESSO = "Double Espresso"
 const val CAPPUCCINO = "Cappuccino"
@@ -10,6 +10,7 @@ const val MOCHA = "Mocha"
 const val FLAT_WHITE = "Flat White"
 const val AMERICANO = "Americano"
 
+// Coffee prices
 const val ESPRESSO_PRICE = 2.5
 const val DOUBLE_ESPRESSO_PRICE = 3.0
 const val CAPPUCCINO_PRICE = 3.0
@@ -17,6 +18,7 @@ const val LATTE_PRICE = 3.5
 const val AMERICANO_PRICE = 2.0
 const val FLAT_WHITE_PRICE = 3.2
 
+// Coffee menu
 val coffeeMenu = listOf(
     ESPRESSO, DOUBLE_ESPRESSO, CAPPUCCINO, LATTE,
     MACCHIATO, MOCHA, FLAT_WHITE, AMERICANO
