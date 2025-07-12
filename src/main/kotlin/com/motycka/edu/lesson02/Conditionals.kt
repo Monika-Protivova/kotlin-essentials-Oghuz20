5cd6311badd0a401b517759a9e1b86cf6538890f
Oghuz Hasanli <oguzhsnli2@gmail.com>
2025-07-12 22:12:05 +0400

    Conditionals.kt

--- src/main/kotlin/com/motycka/edu/lesson02/Conditionals.kt
+++ src/main/kotlin/com/motycka/edu/lesson02/Conditionals.kt
@@ -1,25 +1,5 @@
 package com.motycka.edu.lesson02
 
-val ESPRESSO = "Espresso"
-val DOUBLE_ESPRESSO = "Double Espresso"
-val CAPPUCCINO = "Cappuccino"
-val LATTE = "Latte"
-val MACCHIATO = "Macchiato"
-val MOCHA = "Mocha"
-val FLAT_WHITE = "Flat White"
-val AMERICANO = "Americano"
-
-val coffeeMenu = listOf(
-    ESPRESSO,
-    DOUBLE_ESPRESSO,
-    CAPPUCCINO,
-    LATTE,
-    MACCHIATO,
-    MOCHA,
-    FLAT_WHITE,
-    AMERICANO
-)
-
 fun conditionals() {
     val orders = listOf(
         Triple(1, listOf(ESPRESSO, CAPPUCCINO, LATTE, AMERICANO), 9.0),
