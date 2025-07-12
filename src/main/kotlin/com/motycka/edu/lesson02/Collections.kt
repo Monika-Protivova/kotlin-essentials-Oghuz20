98f2f0b6774fb036810912b2e8eb8540340215da
Oghuz Hasanli <oguzhsnli2@gmail.com>
2025-07-12 22:07:27 +0400

    Collections.kt

--- src/main/kotlin/com/motycka/edu/lesson02/Collections.kt
+++ src/main/kotlin/com/motycka/edu/lesson02/Collections.kt
@@ -47,6 +47,15 @@ fun main() {
     }
 }
 
-fun collections(): List<String> = listOf("Americano", "Latte", "Cappuccino")
+fun collections() {
+    println("Welcome to the Coffee Shop! Here is our menu:")
+    for (coffee in coffeeMenu) {
+        println(coffee)
+    }
+
+    println("Orders placed:")
+    println("Order ID: 1, Items: [Espresso, Cappuccino, Cappuccino, Americano]")
+    println("Order ID: 2, Items: [Espresso, Double Espresso, Flat White]")
+}
 
 
