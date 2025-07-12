6aa96f28371781f1b45652a5d55ded6124760bd1
Oghuz Hasanli <oguzhsnli2@gmail.com>
2025-07-12 22:36:39 +0400

    Variables.kt

--- src/main/kotlin/com/motycka/edu/lesson02/Variables.kt
+++ src/main/kotlin/com/motycka/edu/lesson02/Variables.kt
@@ -1,8 +1,5 @@
 package com.motycka.edu.lesson02
 
-const val espressoPrice = 2.5
-const val cappuccinoPrice = 3.0
-
 fun main(args: Array<String>) {
     val espressoQuantity = args.count { it == ESPRESSO }
     val cappuccinoQuantity = args.count { it == CAPPUCCINO }
