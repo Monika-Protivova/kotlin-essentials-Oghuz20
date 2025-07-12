4223b6564cdf2d095a48aedd89767ce34442528f
Oghuz Hasanli <oguzhsnli2@gmail.com>
2025-07-12 21:39:40 +0400

    Variables.kt

--- src/main/kotlin/com/motycka/edu/lesson02/Variables.kt
+++ src/main/kotlin/com/motycka/edu/lesson02/Variables.kt
@@ -5,10 +5,11 @@ const val cappuccino: String = "Cappuccino"
 const val espressoPrice: Double = 2.5
 const val cappuccinoPrice: Double = 3.0
 
+val variables = listOf(espresso, cappuccino, espressoPrice, cappuccinoPrice)
+
 fun main(args: Array<String>) {
     val espressoQuantity = args.count { it == espresso }
     val cappuccinoQuantity = args.count { it == cappuccino }
     val totalPrice = (espressoQuantity * espressoPrice) + (cappuccinoQuantity * cappuccinoPrice)
     println(totalPrice)
 }
-val variables = listOf(espresso, cappuccino, espressoPrice, cappuccinoPrice)
