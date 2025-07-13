ab115ff1e491bf5af64bb370007efa5e4bae8f77
Oghuz Hasanli <oguzhsnli2@gmail.com>
2025-07-13 17:52:14 +0400

    Rectangle.kt

--- src/main/kotlin/com/motycka/edu/lesson03/model/Rectangle.kt
+++ src/main/kotlin/com/motycka/edu/lesson03/model/Rectangle.kt
@@ -1,20 +1,11 @@
+// Lesson 03 - Rectangle Model
 package com.motycka.edu.lesson03.model
 
-import com.motycka.edu.lesson03.Quadrilateral
-import com.motycka.edu.lesson03.Shape2D
-import com.motycka.edu.lesson03.Shape3D
-
 open class Rectangle(
-    private val _width: Double,
-    private val _length: Double
+    override val width: Double,
+    override val length: Double
 ) : Shape2D, Quadrilateral {
-
-    // Public overrides for interface properties
-    override val width: Double get() = _width
-    override val length: Double get() = _length
-
-    override fun area(): Double = _width * _length
-    override fun perimeter(): Double = 2 * (_width + _length)
-
-    override fun to3D(depth: Double): Shape3D = Cuboid(_width, _length, depth)
+    override fun area(): Double = width * length
+    override fun perimeter(): Double = 2 * (width + length)
+    override fun to3D(depth: Double): Shape3D = Cuboid(width, length, depth)
 }
