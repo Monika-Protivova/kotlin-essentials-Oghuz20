ac1a7a4f396234361ecadb9aeb5c27325c9a4d3d
Oghuz Hasanli <oguzhsnli2@gmail.com>
2025-07-13 17:54:26 +0400

    Sphere.kt

--- src/main/kotlin/com/motycka/edu/lesson03/model/Sphere.kt
+++ src/main/kotlin/com/motycka/edu/lesson03/model/Sphere.kt
@@ -1,9 +1,9 @@
 package com.motycka.edu.lesson03.model
 
-import com.motycka.edu.lesson03.Shape3D
 import kotlin.math.PI
+import kotlin.math.pow
 
-class Sphere(private val radius: Double) : Shape3D {
-    override fun volume(): Double = (4.0 / 3.0) * PI * radius * radius * radius
-    override fun surfaceArea(): Double = 4 * PI * radius * radius
+class Sphere(val radius: Double) : Shape3D {
+    override fun volume(): Double = (4.0 / 3.0) * PI * radius.pow(3)
+    override fun surfaceArea(): Double = 4 * PI * radius.pow(2)
 }
