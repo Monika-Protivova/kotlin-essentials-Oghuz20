4ebc4fb0528a8510a116b0fe415a669f04f008ea
Oghuz Hasanli <oguzhsnli2@gmail.com>
2025-07-13 18:16:58 +0400

    Square.kt

--- src/main/kotlin/com/motycka/edu/lesson03/model/Square.kt
+++ src/main/kotlin/com/motycka/edu/lesson03/model/Square.kt
@@ -1,6 +1,6 @@
 package com.motycka.edu.lesson03.model
 
 class Square(val side: Double) : Rectangle(side, side) {
-    override fun to3D(depth: Double): Shape3D = Cuboid(side, side, height)
+    override fun to3D(depth: Double): Shape3D = Cuboid(side, side, depth)
     fun to3D(): Shape3D = to3D(side)
 }
